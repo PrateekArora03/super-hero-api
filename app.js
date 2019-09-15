b6adb4ea8ad2744eeabd2bc7adaed7f40f165fd5
prarora98 <prateekisthebest98@gmail.com>
2019-09-16 02:22:53 +0530

    super-hero api done

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 const indexRoutes = require("./routes/index");
 
-mongoose.connect("mongodb:localhost/test", err => {
+mongoose.connect(process.env.mongodb_key, { useNewUrlParser: true }, err => {
   err ? console.log(err) : console.log("connected to DB!");
 });
 
-app.use("/api", indexRoutes);
+app.use("/api/hero-creater", indexRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router();
 const Hero = require("../model/hero");
 
+//to get superHero
 router.get("/:hero", (req, res, next) => {
-  let hero = req.params.hero;
-  Hero.findOne({ name: hero }, (err, getHero) => {
+  let name = req.params.hero;
+  Hero.findOne({ name }, (err, getHero) => {
     if (err) return next(err);
     if (!getHero) {
       return res
@@ -12,6 +13,37 @@ router.get("/:hero", (req, res, next) => {
         .json({ status: "fail", message: "Hero not found" });
     }
     return res.status(200).json({ status: "success", hero: getHero });
+  });
+});
+
+//to create superHero
+router.post("/", (req, res, next) => {
+  Hero.create(req.body, (err, hero) => {
+    if (err) return next(err);
+    return res.status(201).json({ status: "success", hero });
+  });
+});
+
+//to update superHero
+router.put("/:hero", (req, res, next) => {
+  let name = req.params.hero;
+  Hero.findOneAndUpdate(
+    { name },
+    req.body,
+    { new: true },
+    (err, updatedHero) => {
+      if (err) return next(err);
+      return res.status(201).json({ status: "success", updatedHero });
+    }
+  );
+});
+
+//to delete superHero
+router.delete("/:hero", (req, res, next) => {
+  let name = req.params.hero;
+  Hero.findOneAndDelete({ name }, (err, deletedHero) => {
+    if (err) return next(err);
+    return res.status(201).json({ status: "success", deletedHero });
   });
 });
 
