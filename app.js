1eebffbebeb28f10ab544856867c92632f9a7891
prarora98 <prateekisthebest98@gmail.com>
2019-09-16 02:01:05 +0530

    superhero add route file and add route to app.js

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+
+const indexRoutes = require("./routes/index");
+
+mongoose.connect("mongodb:localhost/test", err => {
+  err ? console.log(err) : console.log("connected to DB!");
+});
+
+app.use("/api", indexRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
