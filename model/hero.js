9f19454e5646308fa864b94e8cc118257329c429
prarora98 <prateekisthebest98@gmail.com>
2019-09-16 00:35:09 +0530

    superhero schema done

--- model/hero.js
+++ model/hero.js
@@ -1,1 +1,12 @@
-const mongoose = require(mongoose);
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const heroSchema = new Schema({
+  name: String,
+  photo: String,
+  hiddenIdentity: String, //if Array but it like [String] or Array
+  power: Array
+});
+
+const Hero = mongoose.model("Hero", heroSchema);
+
+module.exports = Hero;
